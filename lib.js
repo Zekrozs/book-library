5460f0361d721cda9630d341524ae14ff915b601
Zaid Shuqair <zaidshuqairbizz@gmail.com>
2026-08-14 14:40:02 +0300

    rewrote dialog EL with ED added DOM object

--- lib.js
+++ lib.js
@@ -1,3 +1,28 @@
-'use strict'
-let box = document.getElementById('dialog-box')
-box.showModal()
+    'use strict'
+
+let DOM ={
+    openDialog: document.getElementById('open-dialog'),
+    window: document.getElementById('main-page'),
+    dialogBox: document.getElementById('dialog-box')
+}
+
+
+
+
+
+
+
+
+  
+  
+    DOM.window.addEventListener('click', e => {
+        const target = e.target
+
+        const dialogBtn = target.closest('[data-button="open-dialog"]')
+
+        if (dialogBtn){
+            DOM.dialogBox.showModal()
+        }
+        
+   
+    })
